1f6d4a2a10f7f2a0547b39710045590d9cf9cad0
Vishal_Nagali <vishalnagali@gnail.com>
2023-08-02 12:41:37 +0530

    Changes have been committed.

---
+++ Table.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Coulumns from './Coulumns'
+
+function Table() {
+  return (
+    <table>
+        <tbody>
+            <tr>
+            <Coulumns></Coulumns>
+            </tr>
+        </tbody>
+    </table>
+  )
+}
+
+export default Table
